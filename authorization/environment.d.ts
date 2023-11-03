0a2b55f90d0e1eb5df514afc2e9aa01d9a535fcb
Rishad <armreshad@gmail.com>
2023-11-04 02:37:00 +0300

    enviroment variables types added

--- authorization/environment.d.ts
+++ authorization/environment.d.ts
@@ -4,6 +4,9 @@ declare global {
       EMAIL_SECRET_TOKEN: string;
       ACCESS_SECRET_TOKEN: string;
       REFRESH_SECRET_TOKEN: string;
+      MYSQL_HOST: string;
+      MYSQL_ROOT_PASSWORD: string;
+      MYSQL_DATABASE: string;
     }
   }
 }
